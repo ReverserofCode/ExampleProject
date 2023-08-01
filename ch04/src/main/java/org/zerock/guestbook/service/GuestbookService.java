59ec1f4eedade6b22e829133b7b3caa8802e53a5
ReverserofCode <patteon@naver.com>
2023-08-01 16:22:40 +0900

    검색 쿼리 예시 프로젝트

--- ch04/src/main/java/org/zerock/guestbook/service/GuestbookService.java
+++ ch04/src/main/java/org/zerock/guestbook/service/GuestbookService.java
@@ -1,7 +1,18 @@
 package org.zerock.guestbook.service;
 
 import org.zerock.guestbook.dto.GuestDto;
+import org.zerock.guestbook.entity.Guestbook;
 
 public interface GuestbookService {
     Long register(GuestDto dto);
+
+    default Guestbook dtoToEntity(GuestDto guestDto){
+        Guestbook entity = Guestbook.builder()
+                .gno(guestDto.getGno())
+                .title(guestDto.getTitle())
+                .content(guestDto.getContent())
+                .writer(guestDto.getWriter())
+                .build();
+        return entity;
+    }
 }
--- ch04/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ ch04/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -3,6 +3,7 @@ package org.zerock.guestbook.service;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.zerock.guestbook.dto.GuestDto;
+import org.zerock.guestbook.entity.Guestbook;
 
 @Log4j2
 @Service
@@ -10,6 +11,12 @@ public class GuestbookServiceImpl implements GuestbookService{
 
     @Override
     public Long register(GuestDto dto) {
+
+        log.info("DTO------------------------------");
+        log.info(dto);
+        Guestbook entity = dtoToEntity(dto);
+        log.info(entity);
+
         return null;
     }
 }
